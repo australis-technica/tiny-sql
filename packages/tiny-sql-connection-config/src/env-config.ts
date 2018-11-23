ab4aa7cfc5ff4755811d60cc30c65bc65cdc802d
me <danieliozzi@gmail.com>
2018-11-23 11:52:59 +0200

    make space ...

---
+++ packages/tiny-sql-connection-config/src/env-config.ts
@@ -0,0 +1,19 @@
+import { ConnectionConfig } from "tedious";
+import parseString from "@australis/tiny-sql-connection-string-parse";
+import { debugModule } from "@australis/create-debug";
+const debug = debugModule(module);
+/** */
+const _cached: { [envKey: string]: ConnectionConfig } = {};
+/** */
+export default (envKey = "DB") => {
+  if (_cached[envKey]) return _cached[envKey];
+  // ...
+  const connectionString = process.env[envKey];
+  if (!connectionString) {
+    throw new Error(`process.env.${envKey} is NOT set!`);
+  }
+  // ...
+  _cached[envKey] = parseString(connectionString);
+  debug("Using %s/%s", _cached[envKey].server, _cached[envKey].options.database);
+  return _cached[envKey];
+};
