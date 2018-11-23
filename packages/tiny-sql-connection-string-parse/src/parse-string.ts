f1be7446cb1453c70c1644a50f61962046a289ec
Daniel <danieliozzi@gmail.com>
2018-11-23 07:56:53 +0200

    improve parse-string  ...begin allow more keywords be less strict

--- packages/tiny-sql-connection-string-parse/src/parse-string.ts
+++ packages/tiny-sql-connection-string-parse/src/parse-string.ts
@@ -1,38 +1,81 @@
 import { ConnectionConfig } from "tedious";
-import { isString } from "util";
 import { debugModule } from "@australis/create-debug";
-const debug  = debugModule(module);
-/** 
- * sample 'Data Source=(local);Initial Catalog=DBNAME;user=userName;password=password;'
- * */
-export default function parseString(connectionString: string): ConnectionConfig {
+const debug = debugModule(module);
+/** */
+export default function parseString(
+  connectionString: string,
+): ConnectionConfig {
   try {
-    if (connectionString.indexOf("Integrated Security") !== -1) {
-      throw new Error("Integrated Security is Not Implemented");
-    }
-    const config = connectionString.split(";")
-      .filter(x => isString(x) && x.trim() !== "")
-      .reduce((out, next) => {
-        const parts = next.split("=");
-        return Object.assign(out, { [parts[0]]: parts[1] });
-      }, {} as { [key: string]: string });
-    const server = config["Data Source"].split(":")[0].replace("(local)", "localhost");
-    const port = Number(config["Data Source"].split(":")[1]) || 1433
-    const database = config["Initial Catalog"];
-    // non standard?
-    const encrypt = Boolean(config.encrypt);
+    const {
+      server,
+      userName,
+      password,
+      database,
+      port,
+      encrypt,
+    } = sqlClientLike(connectionString);
     return {
       server,
-      userName: config["user"],
-      password: config["password"],
+      userName,
+      password,
       options: {
         database,
         port,
-        encrypt
-      }
-    }
+        encrypt,
+      },
+    };
   } catch (error) {
     debug("can't parse connection-string: %s", error.message);
     return undefined;
   }
+}
+/**
+ * Parse a subset of a .Net framework/SqlClient like connection string
+ * & extra keywords
+ *  as ';' separated key=value entries
+ * sample 'Data Source=(local);Initial Catalog=DBNAME;user=userName;password=password;'
+ * */
+function sqlClientLike(connectionString: string) {
+  if (connectionString.indexOf("Integrated Security") !== -1) {
+    throw new Error("Integrated Security is Not Implemented");
+  }
+  
+  function find(keyToFind: string | RegExp) {
+    if (typeof keyToFind === "string")
+      /** */
+      return (key: string) =>
+        key && key.toLowerCase().trim() === keyToFind.toLowerCase();
+    return (key: string) => keyToFind.test(key);
+  }
+
+  const config = connectionString
+    .split(";")
+    .filter(x => x && typeof x === "string" && x.trim() !== "")
+    .reduce(
+      (out, next) => {
+        const parts = next.split("=");
+        return Object.assign(out, { [parts[0]]: parts[1] });
+      },
+      {} as { [key: string]: string },
+    );
+
+  const keys = Object.keys(config);
+
+  let server = config[keys.find(find(/(data(\s+)?source|server)/i))];
+  const port = Number(server && server.split(/,|:/)[1]);
+  server = server && server.split(/,|:/)[0];  
+  server = server && server.replace(/(\.|\(local\))/i, "localhost");  
+  const database = config[keys.find(find(/(initial(\s+)?catalog|database)/i))];
+  const userName = config[keys.find(find(/(user)/i))];
+  const password = config[keys.find(find(/(password)/i))];
+  // non standard?
+  const encrypt = Boolean(config.encrypt);
+  return {
+    server,
+    port,
+    database,
+    encrypt,
+    userName,
+    password,
+  };
 }
--- packages/tiny-sql-connection-string-parse/test/index.test.ts
+++ packages/tiny-sql-connection-string-parse/test/index.test.ts
@@ -1,20 +1,52 @@
 describe("parse-connection-string", () => {
   it("works", async () => {
-      const parseString = (await import("../src")).default;
-      const connectionConfig = parseString("Data Source=localhost;user=sa;password=P@55w0rd!;Initial Catalog=MyDb");
-      expect(connectionConfig.server).toEqual("localhost");
-      expect(connectionConfig.userName).toEqual("sa");
-      expect(connectionConfig.password).toEqual("P@55w0rd!");
-      expect(connectionConfig.options.database).toEqual("MyDb");
-      expect(connectionConfig.options.encrypt).toBe(false);
-  })
+    const parseString = (await import("../src")).default;
+    const connectionConfig = parseString(
+      "Data Source=localhost,1444;user=sa;password=P@55w0rd!;Initial Catalog=MyDb",
+    );
+    expect(connectionConfig.server).toEqual("localhost");
+    expect(connectionConfig.userName).toEqual("sa");
+    expect(connectionConfig.password).toEqual("P@55w0rd!");
+    expect(connectionConfig.options.database).toEqual("MyDb");
+    expect(connectionConfig.options.encrypt).toBe(false);
+    expect(connectionConfig.options.port).toBe(1444);
+    expect(
+        parseString("Data Source=localhost:1444").options.port
+    ).toBe(1444)
+    expect(
+        parseString("DataSource=localhost").server
+    ).toBe("localhost")
+    expect(
+        parseString("DataSource=.").server
+    ).toBe("localhost")
+    expect(
+        parseString("server=.").server
+    ).toBe("localhost")
+    expect(
+        parseString("DataSource=(local)").server
+    ).toBe("localhost");
+    expect(
+        parseString("Initial Catalog=db").options.database
+    ).toBe("db");
+    expect(
+        parseString("Initial        Catalog=db").options.database
+    ).toBe("db");
+    expect(
+        parseString("InitialCatalog=db").options.database
+    ).toBe("db");
+    expect(
+        parseString("database=db").options.database
+    ).toBe("db");
+  });
   it("works1", async () => {
     const parseString = (await import("../src")).default;
-    const connectionConfig = parseString("Data Source=localhost;user=sa;password=P@55w0rd!;Initial Catalog=MyDb;encrypt=true");
+    const connectionConfig = parseString(
+      "Data Source=localhost;user=sa;password=P@55w0rd!;Initial Catalog=MyDb;encrypt=true",
+    );
     expect(connectionConfig.server).toEqual("localhost");
     expect(connectionConfig.userName).toEqual("sa");
     expect(connectionConfig.password).toEqual("P@55w0rd!");
     expect(connectionConfig.options.database).toEqual("MyDb");
     expect(connectionConfig.options.encrypt).toBe(true);
-})
-})
+  });
+});
