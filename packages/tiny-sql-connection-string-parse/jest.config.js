a06826c65a9cff71cef7953f6e62c04442205872
Daniel <danieliozzi@gmail.com>
2018-11-23 06:43:25 +0200

    clean

--- packages/tiny-sql-connection-string-parse/jest.config.js
+++ packages/tiny-sql-connection-string-parse/jest.config.js
@@ -1,7 +1,3 @@
-// jest.config.js
 module.exports = {
-  preset: "ts-jest",
-  // transform: { "^.+\\.tsx?$": "ts-jest" },
-  // testRegex: "(/test/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  // moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  preset: "ts-jest",  
 };
